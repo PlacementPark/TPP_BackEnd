4c43852907cd61984d50624bb5119343b985df7c
Bala <malharikingdom9@gmail.com>
2024-12-10 23:12:52 +0530

    10/12 -2

--- controllers/candidate.js
+++ controllers/candidate.js
@@ -472,7 +472,7 @@ const getAllByClass = async (req, res) => {
   } else if (type === "InterviewScheduled") {
     query = {
       interviewStatus: {
-        $in: "TPP Venue,Client Venue,Pending FSR,Pending Amcat,Pending Versant,Pending Technical,Pending Typing,Pending Group Discussion,Pending Ops/Client,Pending Vice President".split(
+        $in: "Pending FSR,Pending Amcat,Pending Versant,Pending Technical,Pending Typing,Pending Group Discussion,Pending Ops/Client,Pending Vice President".split(
           ","
         ),
       },
@@ -498,7 +498,7 @@ const getAllByClass = async (req, res) => {
     };
   } else if (type === "VirtualInterview") {
     query = {
-      interviewStatus: "Virtual Interview",
+      interviewStatus: "TPP Venue,Client Venue,Virtual Interview",
       select: { $in: ["", null] },
     };
   } else if (type === "OfferDrop") {
